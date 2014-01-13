698e0f9392221401adb1b66922f4fc720a2e5fca
Joe Attardi <joe@attardi.net>
2014-01-12 21:10:18 -0500

    Added websocket config.

---
+++ src/main/java/net/thinksincode/tailstreamer/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package net.thinksincode.tailstreamer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/tail").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }    
+    
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+ 
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+
+    }
+}
