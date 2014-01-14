cc58a9c99311a19632aa2f6d7b344186fa748d25
Joe Attardi <joe@attardi.net>
2014-01-14 00:43:39 -0500

    changed to use @Value instead of environment.get

--- src/main/java/net/thinksincode/tailstreamer/TailStreamer.java
+++ src/main/java/net/thinksincode/tailstreamer/TailStreamer.java
@@ -3,22 +3,20 @@ package net.thinksincode.tailstreamer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
@@ -29,15 +27,14 @@ public class TailStreamer implements CommandLineRunner {
     @Autowired
     private FileTailService fileTailService;
     
-    @Autowired
-    private Environment environment;
+    @Value("${nonOptionArgs}")
+    private String nonOptionArgs;
     
     @Override
     public void run(String...args) {
-       String[] nonOptionArgs = environment.getProperty("nonOptionArgs").split(",");
-       
+      String[] nonOptionArgsArr = nonOptionArgs.split(",");  
        // TODO handle no such file here
-      fileTailService.tailFile(nonOptionArgs[0]);
+      fileTailService.tailFile(nonOptionArgsArr[0]);
     }
     
     public static void main(String...args) {
