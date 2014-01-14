a9cf931078a7cedbdcdd5fea14c19bdf9ae23450
Joe Attardi <joe@attardi.net>
2014-01-14 00:12:57 -0500

    Added some command line processing. May soon replace with JOpt-based CommandLinePropertySource.

--- src/main/java/net/thinksincode/tailstreamer/FileTailService.java
+++ src/main/java/net/thinksincode/tailstreamer/FileTailService.java
@@ -3,12 +3,16 @@ package net.thinksincode.tailstreamer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service("fileTailService")
 public class FileTailService {
+    
+    private Logger logger = LoggerFactory.getLogger(FileTailService.class);
     
     @Autowired
     private FileWatcher watcher;
@@ -18,6 +22,8 @@ public class FileTailService {
     
     @Async
     public void tailFile(final String filePath) {
+        logger.info("Tailing " + filePath);
+        
         Path file = Paths.get(filePath);
         reader.openFile(file);
         watcher.watchFile(file);
--- src/main/java/net/thinksincode/tailstreamer/TailStreamer.java
+++ src/main/java/net/thinksincode/tailstreamer/TailStreamer.java
@@ -3,12 +3,22 @@ package net.thinksincode.tailstreamer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
@@ -19,24 +29,67 @@ public class TailStreamer implements CommandLineRunner {
     @Autowired
     private FileTailService fileTailService;
     
+    @Autowired
+    private Environment environment;
+    
     @Override
     public void run(String...args) {
-      fileTailService.tailFile(args[0]);
+       String[] nonOptionArgs = environment.getProperty("nonOptionArgs").split(",");
+       
+       // TODO handle no such file here
+      fileTailService.tailFile(nonOptionArgs[0]);
     }
     
     public static void main(String...args) {
-        if (args.length < 1) {
+        CommandLine commandLine = processArguments(args);
+        if (commandLine == null) {
+            System.exit(1);
+        }
+        
+        if (commandLine.hasOption("h")) {
+            printHelp();
+            System.exit(0);
+        }       
+        
+        String[] fileArgs = commandLine.getArgs();
+                
+        if (fileArgs.length < 1) {
             System.err.println("No file specified.");
             System.exit(1);
         }
         
-        if (!Files.exists(Paths.get(args[0]))) {
-            System.err.println(args[0] + ": File not found.");
+        if (!Files.exists(Paths.get(fileArgs[0]))) {
+            System.err.println(fileArgs[0] + ": File not found.");
             System.exit(1);
         }
         
         SpringApplication app = new SpringApplication(TailStreamer.class);
         app.setShowBanner(false);
         app.run(args);
+    }   
+    
+    private static CommandLine processArguments(String...args) {
+        Options options = new Options();
+        options.addOption(OptionBuilder.withLongOpt("server.port")
+                            .withDescription("Listen on PORT")
+                            .hasArg()
+                            .withArgName("PORT")
+                            .create());
+        options.addOption(new Option("h", "Print this message"));
+        
+        CommandLineParser parser = new PosixParser();
+        try {
+            return parser.parse(options, args);
+        } catch (ParseException pe) {
+            System.err.println(pe.getMessage());
+            printHelp();
+            return null;
+        }
+    }
+    
+    private static void printHelp() {
+        System.out.println("Usage: tailstreamer [options] file");
+        System.out.println("  -h                     Print this message");
+        System.out.println("     --server.port=PORT  Listen on PORT (default 8080)");
     }
 }
