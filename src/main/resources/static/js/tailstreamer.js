e4fdb89aed0e7028aad62c0b62d9bc2d9738363e
Joe Attardi <joe@attardi.net>
2014-01-19 23:28:16 -0500

    Added jQuery. Added CSS styling. Improved JavaScript to autoscroll to bottom.

--- src/main/resources/static/js/tailstreamer.js
+++ src/main/resources/static/js/tailstreamer.js
@@ -1,16 +1,28 @@
-var socket = new SockJS("/tail");
-var stompClient = Stomp.over(socket);
-
-stompClient.connect({}, function(frame) {
-	console.log("Connected", frame);
-	stompClient.subscribe("/topic/log", updateLog);
+$(function() {
+	connect();
 });
 
+/**
+ * Connects to the WebSocket endpoint and subscribes to log update
+ * messages.
+ */
+function connect() {
+	var socket = new SockJS("/tail");
+	var stompClient = Stomp.over(socket);	
+	
+	stompClient.connect({}, function(frame) {
+		console.log("Connected", frame);
+		stompClient.subscribe("/topic/log", updateLog);
+	});	
+}
+
+/**
+ * Handles a new log entry.
+ * @param content The message received over the socket
+ */
 function updateLog(content) {
 	var contentDiv = document.createElement("div");
-	contentDiv.innerHTML = content.body;
-	
-	console.log(content);
-	
-	document.getElementById("logContent").appendChild(contentDiv);
+	$(contentDiv).html(content.body);
+	$("#logContent").append(contentDiv);
+	window.scrollTo(0, document.body.scrollHeight);
 }
