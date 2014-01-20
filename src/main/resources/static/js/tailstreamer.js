3e08ee646619bd99948357055fa49615b277c0a9
Joe Attardi <joe@attardi.net>
2014-01-20 00:54:32 -0500

    Added logo. Added live connection status indicator.

--- src/main/resources/static/js/tailstreamer.js
+++ src/main/resources/static/js/tailstreamer.js
@@ -4,6 +4,9 @@ $(function() {
 	$(window).resize(sizeLogContentArea);
 });
 
+/**
+ * Recalculates the proper size of the log content area.
+ */
 function sizeLogContentArea() {
 	$("#logContent").height($(window).height() - 85);
 }
@@ -16,9 +19,18 @@ function connect() {
 	var socket = new SockJS("/tail");
 	var stompClient = Stomp.over(socket);	
 	
+	$("#connectionStatus").html("Connecting");
 	stompClient.connect({}, function(frame) {
+		$("#connectionStatus").html("Connected");
 		stompClient.subscribe("/topic/log", updateLog);
 	});	
+	
+	// stomp.js has to perform cleanup on close, but we need to listen too
+	var stompCleanup = socket.onclose;
+	socket.onclose = function() {
+		$("#connectionStatus").html("Disconnected");
+		stompCleanup();
+	}
 }
 
 /**
