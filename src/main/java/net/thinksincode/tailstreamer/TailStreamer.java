1ee9e302ab3acac235f627ded432d978479accc3
Joe Attardi <joe@attardi.net>
2014-01-19 22:03:26 -0500

    Refactored argument processing into its own class, ArgumentProcessor. Added argument processing tests.

--- src/main/java/net/thinksincode/tailstreamer/TailStreamer.java
+++ src/main/java/net/thinksincode/tailstreamer/TailStreamer.java
@@ -29,23 +29,26 @@ public class TailStreamer implements CommandLineRunner {
     }
 
     public static void main(String...args) {
-        TailStreamerApplication app = new TailStreamerApplication(TailStreamer.class);
-        app.parseArguments(args);
-        
-        if (app.parseArguments(args) && app.validateArguments()) {
-            if (app.getOptions().has("h")) {
-                printHelp();
+        ArgumentProcessor argumentProcessor = new ArgumentProcessor();
+        if (argumentProcessor.parseArguments(args) && argumentProcessor.validateArguments()) {
+            if (argumentProcessor.getOptions().has("h")) {
+                System.out.println(getHelpText());
             } else {
+                TailStreamerApplication app = new TailStreamerApplication(TailStreamer.class, argumentProcessor.getOptions());
                 app.run(args);
             }
         } else {
-            System.err.println(app.getValidationErrorMessage());
+            System.err.println(argumentProcessor.getValidationErrorMessage());
+            System.exit(1);
         }
     }   
     
-    private static void printHelp() {
-        System.out.println("Usage: tailstreamer [options] file");
-        System.out.println("  -h                     Print this message");
-        System.out.println("     --server.port=PORT  Listen on PORT (default 8080)");
+    public static String getHelpText() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Usage: tailstreamer [options] file");
+        builder.append("  -h                     Print this message");
+        builder.append("     --server.port=PORT  Listen on PORT (default 8080)");
+        
+        return builder.toString();
     }
 }
--- src/main/java/net/thinksincode/tailstreamer/TailStreamerApplication.java
+++ src/main/java/net/thinksincode/tailstreamer/TailStreamerApplication.java
@@ -1,11 +1,5 @@
 package net.thinksincode.tailstreamer;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-import joptsimple.OptionException;
-import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 import org.springframework.boot.SpringApplication;
@@ -14,55 +8,11 @@ import org.springframework.core.env.JOptCommandLinePropertySource;
 import org.springframework.core.env.MutablePropertySources;
 
 public class TailStreamerApplication extends SpringApplication {
-
-    public static final String MESSAGE_NO_FILE_SPECIFIED = "No file specified";
-    public static final String MESSAGE_FILE_NOT_FOUND = "File not found: %s";
-    public static final String MESSAGE_INVALID_OPTION = "Unknown option: %s";
-    
     private OptionSet options;
-    private String validationErrorMessage;
     
-    public boolean parseArguments(String...args) {
-        OptionParser parser = new OptionParser();
-        parser.accepts("server.port").withRequiredArg();
-        parser.accepts("h");
-        parser.nonOptions("file to watch").ofType(File.class);
-        
-        try {
-            options = parser.parse(args);
-            return true;
-        } catch (OptionException oe) {
-            String firstOption = new ArrayList<String>(oe.options()).get(0);
-            validationErrorMessage = String.format(MESSAGE_INVALID_OPTION, firstOption);
-            return false;
-        }
-    }
-    
-    public boolean validateArguments() { 
-        List<?> nonOptionArgs = options.nonOptionArguments();
-        if (options.has("h")) {
-            return true;
-        } else if (nonOptionArgs.isEmpty()) {
-            validationErrorMessage = MESSAGE_NO_FILE_SPECIFIED;
-            return false;
-        } else if (!((File) nonOptionArgs.get(0)).exists()) {
-            validationErrorMessage = String.format(MESSAGE_FILE_NOT_FOUND, nonOptionArgs.get(0));
-            return false;
-        }
-        
-        return true;
-    }
-    
-    public OptionSet getOptions() {
-        return options;
-    }
-    
-    public String getValidationErrorMessage() { 
-        return validationErrorMessage;
-    }
-    
-    public TailStreamerApplication(Class<?> source) {
+    public TailStreamerApplication(Class<?> source, OptionSet options) {
         super(source);
+        this.options = options;
         setShowBanner(false);
         setAddCommandLineProperties(false);
     }
