b0e98872d886f0ae11625319dacf3337e5a8d30c
Joe Attardi <joe@attardi.net>
2014-01-21 00:55:48 -0500

    Added Font Awesome. Added connection status icons. Added reconnect attempt when connection lost.

--- src/main/resources/static/js/tailstreamer.js
+++ src/main/resources/static/js/tailstreamer.js
@@ -1,5 +1,5 @@
 $(function() {
-	connect();
+	connect(true);
 	sizeLogContentArea();
 	$(window).resize(sizeLogContentArea);
 });
@@ -15,21 +15,44 @@ function sizeLogContentArea() {
  * Connects to the WebSocket endpoint and subscribes to log update
  * messages.
  */
-function connect() {
+function connect(reconnect) {
 	var socket = new SockJS("/tail");
 	var stompClient = Stomp.over(socket);	
 	
-	$("#connectionStatus").html("Connecting");
+	setConnectionState(SockJS.CONNECTING);
 	stompClient.connect({}, function(frame) {
-		$("#connectionStatus").html("Connected");
+		setConnectionState(SockJS.CONNECTED);
 		stompClient.subscribe("/topic/log", updateLog);
 	});	
 	
 	// stomp.js has to perform cleanup on close, but we need to listen too
 	var stompCleanup = socket.onclose;
 	socket.onclose = function() {
-		$("#connectionStatus").html("Disconnected");
+		setConnectionState(SockJS.CLOSED);
 		stompCleanup();
+		if (reconnect) {
+			retryConnection();
+		}
+	}
+}
+
+function retryConnection() {
+	connect(false);
+}
+
+function setConnectionState(state) {
+	var connectionStatus = $("#connectionStatus");
+	
+	switch (state) {
+		case SockJS.CLOSED:
+			connectionStatus.html('<i class="disconnected fa fa-exclamation-triangle"></i> Disconnected');
+			break;
+		case SockJS.CONNECTING: 
+			connectionStatus.html('<img style="vertical-align: middle; margin-bottom: 2px;" src="connecting.gif"> Connecting');
+			break;
+		case SockJS.CONNECTED:
+			connectionStatus.html('<i class="connected fa fa-check-square"></i> Connected');
+			break;
 	}
 }
 
