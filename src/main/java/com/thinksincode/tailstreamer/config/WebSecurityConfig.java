4df7d07f824cdb1cadb613a1b01b88c6e8cc3a2d
Joe Attardi <joe@attardi.net>
2014-05-02 00:43:55 -0400

    Added favicon to login page.

--- src/main/java/com/thinksincode/tailstreamer/config/WebSecurityConfig.java
+++ src/main/java/com/thinksincode/tailstreamer/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/css/*.css", "/fonts/*", "/logo.png").permitAll()
+                .antMatchers("/css/*.css", "/fonts/*", "/logo.png", "/favicon.png").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .formLogin()
