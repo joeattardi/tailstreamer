879581365b2149924b6ac009bbc295dddd5f73ab
Joe Attardi <joe@attardi.net>
2014-05-01 22:11:57 -0400

    Updated version to 0.1.2.

--- src/main/java/com/thinksincode/tailstreamer/TailStreamer.java
+++ src/main/java/com/thinksincode/tailstreamer/TailStreamer.java
@@ -13,6 +13,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAutoConfiguration
 @ComponentScan
 public class TailStreamer implements CommandLineRunner {
+    public static final String VERSION = "0.1.2";
+
     @Autowired
     private FileTailService fileTailService;
     
