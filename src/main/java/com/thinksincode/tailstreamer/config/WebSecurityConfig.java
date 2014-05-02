481dd1af027ef71bf590c9432f983422c8366ec2
Joe Attardi <joe@attardi.net>
2014-05-01 23:40:46 -0400

    Added minimal authentication support. Spring-generated login form, hardcoded username and password.

---
+++ src/main/java/com/thinksincode/tailstreamer/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.thinksincode.tailstreamer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER");
+    }
+}
