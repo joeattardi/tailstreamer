ba1a311642b85c2f8782b9d85a004e9d4d48b936
Joe Attardi <joe@attardi.net>
2014-08-02 23:30:51 -0400

    Improve configurable SSL support

--- src/main/java/com/thinksincode/tailstreamer/config/TomcatConfig.java
+++ src/main/java/com/thinksincode/tailstreamer/config/TomcatConfig.java
@@ -1,67 +1,49 @@
 package com.thinksincode.tailstreamer.config;
 
 import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
-
-import java.io.FileNotFoundException;
 
 @Configuration
 public class TomcatConfig {
-    @Bean
-    public EmbeddedServletContainerCustomizer containerCustomizer(@Value("${server.port:8080}") final int port,
-        @Value("${ssl.keystore.file:null}") final String keystoreFile, @Value("${ssl.keystore.password:null}") final String keystorePassword,
-        @Value("${ssl.keystore.type:null}") final String keystoreType, @Value("${ssl.keystore.alias:null}") final String keystoreAlias,
-        @Value("${ssl.enable:false}") final boolean useSSL
-    ) throws FileNotFoundException {
-        final String absoluteKeystoreFile = ResourceUtils.getFile(keystoreFile).getAbsolutePath();
+    private final static Log logger = LogFactory.getLog(TomcatConfig.class);
 
-        return new EmbeddedServletContainerCustomizer() {
-            @Override
-            public void customize(ConfigurableEmbeddedServletContainer container) {
-                TomcatEmbeddedServletContainerFactory containerFactory = (TomcatEmbeddedServletContainerFactory) container;
-                containerFactory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
-                    @Override
-                    public void customize(Connector connector) {
-                        if (useSSL) {
-                            connector.setSecure(true);
-                            connector.setScheme("https");
-                            connector.setAttribute("keystoreFile", absoluteKeystoreFile);
-                            connector.setAttribute("keystorePass", keystorePassword);
-                            connector.setAttribute("keystoreType", keystoreType);
-                            connector.setAttribute("keyAlias", keystoreAlias);
-                            connector.setAttribute("clientAuth", "false");
-                            connector.setAttribute("sslProtocol", "TLS");
-                            connector.setAttribute("SSLEnabled", true);
-                        }
-                        connector.setPort(port);
-                    }
-                });
-            }
-        };
-//
-////        return (ConfigurableEmbeddedServletContainer factory) -> {
-//              return new ConfigurableEmbeddedServletContainer() {
-//              @Override
-//              public void customize(Con)
-//            TomcatEmbeddedServletContainerFactory containerFactory = (TomcatEmbeddedServletContainerFactory) factory;
-//            containerFactory.addConnectorCustomizers((TomcatConnectorCustomizer) (Connector connector) -> {
-//                connector.setSecure(true);
-//                connector.setScheme("https");
-//                connector.setAttribute("keystoreFile", absoluteKeystoreFile);
-//                connector.setAttribute("keystorePass", keystorePassword);
-//                connector.setAttribute("keystoreType", keystoreType);
-//                connector.setAttribute("keyAlias", keystoreAlias);
-//                connector.setAttribute("clientAuth", "false");
-//                connector.setAttribute("sslProtocol", "TLS");
-//                connector.setAttribute("SSLEnabled", true);
-//            });
-//        };
+    @Autowired
+    private SSLConfig sslConfig;
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+        if (sslConfig != null && sslConfig.isEnable()) {
+            tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        }
+
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        if (sslConfig == null) {
+            throw new IllegalStateException("SSL configuration not specified");
+        }
+
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        connector.setScheme("https");
+        connector.setSecure(true);
+        connector.setPort(sslConfig.getPort());
+        protocol.setSSLEnabled(sslConfig.isEnable());
+        protocol.setKeystoreFile(sslConfig.getKeystore());
+        protocol.setKeystorePass(sslConfig.getKeystorePassword());
+        protocol.setKeyAlias(sslConfig.getKeyAlias());
+
+        logger.info(String.format("Initializing SSL connector on port %d", sslConfig.getPort()));
+        return connector;
     }
 }
