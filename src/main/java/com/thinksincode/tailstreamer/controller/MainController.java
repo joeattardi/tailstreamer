cf501f8434d602db4b607d5cfaefc6cfb55971ef
Joe Attardi <joe@attardi.net>
2015-05-29 20:08:46 -0400

    Truncate filename if it exceeds 50 characters

--- src/main/java/com/thinksincode/tailstreamer/controller/MainController.java
+++ src/main/java/com/thinksincode/tailstreamer/controller/MainController.java
@@ -9,15 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    
+
+    /** The maximum filename length before truncation occurs. */
+    private static final int MAX_FILENAME_LENGTH = 50;
+
     @Autowired
     private FileTailService fileTailService;
     
     @RequestMapping("/")
     public String index(Model model) {
-        model.addAttribute("file", fileTailService.getFileName());
+        model.addAttribute("file", maybeTruncate(fileTailService.getFileName()));
         model.addAttribute("filePath", fileTailService.getFilePath());
         model.addAttribute("version", TailStreamer.VERSION);
         return "index";
+    }
+
+    private String maybeTruncate(String fileName) {
+        if (fileName.length() <= MAX_FILENAME_LENGTH) {
+            return fileName;
+        } else {
+            String extension = fileName.substring(fileName.lastIndexOf('.'));
+            return fileName.substring(0, MAX_FILENAME_LENGTH) + "..." + fileName.substring(fileName.length() - extension.length() - 1);
+        }
     }
 }
